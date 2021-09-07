8a32d8e76f81b14b30bd121bd0d1a4b6fe631dfc
OJ42 <mcdavid42@gmail.com>
2021-09-07 14:45:58 -0600

    updated with full function

--- calc.js
+++ calc.js
@@ -7,33 +7,47 @@ const reader = readline.createInterface({
 
 
 
-reader.question("What would you like to calculate?", function(input){
+reader.question("What would you like to calculate? We can handle + - * / sqrt \nsq(squar 1 number) \ncube(cube a number) \npower(raise a number to another power) \nre(find the remainder when dividing)\n", function(input){
 	tokens = input.split(' ');
 	
 	mathSymbol = tokens[0];
 	num1 = Number(tokens[1]);
 	num2 = Number(tokens[2]);
+	num3 = Number(tokens[3]);
 
 	console.log('mathSymbol', mathSymbol);
 	console.log('num1', num1);
-	console.log('num2', num2);	
-
+	console.log('num2', num2);
+	console.log('num3', num3);	
+	
 if (mathSymbol === "+"){
-		console.log(num1 + num2);
+		console.log(num1 + num2 + num3);
 }
 if (mathSymbol === "-"){
-	console.log(num1 - num2);
+	console.log(num1 - num2 - num3);
 }
 if (mathSymbol === "*"){
-	console.log(num1 * num2);
+	console.log(num1 * num2 * num3);
 }
 if (mathSymbol === "/"){
-	console.log(num1 / num2);
+	console.log(num1 / num2 / num3);
 }
 if (mathSymbol === "sqrt"){
-	console.log(Math.sqrt(num1));
+	console.log(Math.sqrt(num1), Math.sqrt(num2), Math.sqrt(num3));
+}
+if (mathSymbol === "sq"){
+	console.log(Math.pow(num1,2));
+}
+if (mathSymbol === "cube"){
+	console.log(Math.pow(num1,3));
+}
+if (mathSymbol === "power"){
+	console.log(Math.pow(num1,num2));
+}
+if (mathSymbol === "re"){
+	console.log(num1 % num2);
 }
 	// This line closes the connection to the command line interface.
 	reader.close()
-
+//does this update
 });
